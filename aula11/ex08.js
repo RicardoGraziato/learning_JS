55472becd42bada507369930ea5cd19e8d606d0b
Ricardo Graziato <ricardograziato@gmail.com>
2024-05-27 15:04:28 -0300

    Condições e prática.

---
+++ aula11/ex08.js
@@ -0,0 +1,6 @@
+var velocidade = 78
+console.log(`A velocidade do seu carro é ${velocidade} km/h`)
+if (velocidade > 60) {
+    console.log(`Você ultrapassou a velocidade permitida.`)
+}
+console.log(`Sempre use o cinto de segurança.`)
