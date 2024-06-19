e3e88ea85bbbad906ae616db773af2caa4a0523b
Ricardo Graziato <ricardograziato@gmail.com>
2024-06-19 19:23:17 -0300

    Ex Tabuada (Tratamento de exceção básico.)

--- aula14ex/ex17/script.js
+++ aula14ex/ex17/script.js
@@ -2,14 +2,18 @@ function calcular() {
     let numero = document.getElementById('txtnum')
     let tab = document.getElementById('tab')
 
-    let num = Number(numero.value)
-    let resultado
+    if(numero.value.length == 0){
+        window.alert('Campo vazio.')
+    } else{
+        let num = Number(numero.value)
+        let resultado
 
-    tab.innerHTML = ''
-    for(let i=1; i<=10; i++){
-        let item = document.createElement('option')
-        resultado = num*i
-        item.text = `${num} x ${i} = ${resultado}`
-        tab.appendChild(item)
+        tab.innerHTML = ''
+        for(let i=1; i<=10; i++){
+            let item = document.createElement('option')
+            resultado = num*i
+            item.text = `${num} x ${i} = ${resultado}`
+            tab.appendChild(item)
+        }
     }
 }
